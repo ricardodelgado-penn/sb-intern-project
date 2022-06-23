1939938975c45c8c7c133a4d997ab1525cdf48a3
Brett Yoncak <brett.yoncak@ip-192-168-1-27.eu-west-1.compute.internal>
2022-06-23 13:46:07 -0500

    header is working, still needs styling

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,14 +1,38 @@
 <script setup>
-const props = defineProps({ allSports: {type: Array, required: true} })
+import IconBaseball from '../components/icons/IconBaseball.vue';
+import IconBasketball from '../components/icons/IconBasketball.vue';
+import IconFootball from '../components/icons/IconFootball.vue';
+import IconHockey from '../components/icons/IconHockey.vue';
+import { ref } from 'vue';
 
-
-
+const props = defineProps({ allSports: { type: Array, required: true } })
+// let thisComponent = ref(IconBaseball)
+// ^^^tried to using this to make the component :is dynamic, but will circle back to it
 </script>
+
 
 <template>
   <div class="container">
     <div class="sports-wrapper">
-      <p v-for="sport in props.allSports.value" :key="sport.key"> {{ sport.name }} | {{sport.key.toUpperCase()}} </p>
+      <div 
+      class="sport" 
+      v-for="sport in allSports" :key="sport.key"
+      >
+
+      <component class="icon" v-if="sport.key === 'mlb'" 
+      :is="IconBaseball"></component>
+      
+      <component class="icon" v-else-if="sport.key === 'nhl'" 
+      :is="IconHockey" ></component>
+      
+      <component class="icon" v-else-if="sport.key === 'nba'" 
+      :is="IconBasketball"></component>
+      
+      <component class="icon" v-else-if="sport.key === 'nfl'" 
+      :is="IconFootball"></component>
+      
+      {{ sport.key.toUpperCase() }}
+      </div>
     </div>
   </div>
 </template>
@@ -21,5 +45,16 @@ const props = defineProps({ allSports: {type: Array, required: true} })
 .sports-wrapper {
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
+}
+
+.sport {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.icon {
+  margin-right: 10px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,66 +1,51 @@
 <script setup>
-// Use axios for your fetch calls
-
 import HeaderBar from '../components/HeaderBar.vue';
-import { onMounted } from 'vue'
-import { inject } from 'vue'
-import { makeBlock } from '@vue/compiler-core';
+import { onMounted, inject, ref } from 'vue'
 const axios = inject('axios')
 
-
-const allSports = [];
+const allSports = ref([]);
 const events = [];
 const bets = [];
 const teams = [];
-const icons = [{key: 'mlb', icon: '@/public/assets/sports_baseball_FILL0_wght400_GRAD0_opsz48.png'}, {key: 'nfl', icon: ''}];
-
-
 
 const getAllSports = () => {
-axios.get('/all-sports').then(response => {
-    allSports.value = response.data
-    });
-
-    for( let i = 0; i < allSports.length; i++ ){
-        let thisKey = allSports[i].key
-        for(let i = 0; i < icons.length; i++){
-        if(thisKey == icons[i].key){
-        allSports[i]['icon'] = icons[i].icon
-        }
-    }
-    }
-
-    console.log('allSports', allSports)
+    axios.get('/all-sports')
+        .then(response => {
+            allSports.value = response.data
+            console.log('allSports', allSports)
+        })
 }
 
 const getEvents = () => {
-axios.get('/events').then(response => {
-    events.value = response.data
-    });
-
-    console.log('events', events)
+    axios.get('/events')
+        .then(response => {
+            events.value = response.data
+            console.log('events', events)
+        })
 }
 
 const getBets = () => {
-axios.get('/bets').then(response => {
-    bets.value = response.data
-    });
-
-    console.log('bets', bets)
+    axios.get('/bets')
+        .then(response => {
+            bets.value = response.data
+            console.log('bets', bets)
+        })
 }
 
 const getTeams = () => {
-axios.get('/teams').then(response => {
-    teams.value = response.data
-    });
-
-    console.log('teams', teams)
+    axios.get('/teams')
+        .then(response => {
+            teams.value = response.data
+            console.log('teams', teams)
+        })
 }
 
-onMounted(() => getAllSports(), getEvents(), getBets(), getTeams())
-
-
-
+onMounted(() => {
+    getAllSports() 
+    getEvents()
+    getBets()
+    getTeams()
+});
 </script>
 
 <template>
